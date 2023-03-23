19bdc444ac071cae8e47818e0571e75811d6d8be
pjhye0104@gmail.com <pjhye0104@gmail.com>
2023-03-23 16:36:15 +0900

    modal수정

--- src/ModalBasic.jsx
+++ src/ModalBasic.jsx
@@ -3,13 +3,18 @@ import "./App.css";
 
 export default function ModalBasic({ setModalOpen }) {
   // 모달 끄기
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+  //   const closeModal = () => {
+  //     setModalOpen(false);
+  //   };
 
   return (
     <div className="container">
-      <button className="close" onClick={closeModal}>
+      <button
+        className="close"
+        onClick={() => {
+          setModalOpen(false);
+        }}
+      >
         X
       </button>
       <p>모달</p>
